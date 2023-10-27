8cd2f0da3ea2487c1bba5278565f7e7c213e2fc8
DeadVan <nikolozi.dadvani.1@btu.edu.ge>
2023-10-27 19:47:36 +0400

    Initial commit

---
+++ app/src/main/java/com/example/dadvaniexercise/PersonRecyclerAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.dadvaniexercise
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.bumptech.glide.Glide
+
+class PersonRecyclerAdapter(private var persons:List<Person>): RecyclerView.Adapter<PersonRecyclerAdapter.PersonViewHolder>() {
+
+    var onItemClick: ((Person) -> Unit)? = null
+
+    inner class PersonViewHolder(itemView: View) : ViewHolder(itemView){
+
+        val imageView = itemView.findViewById<ImageView>(R.id.imageView)
+        val textView = itemView.findViewById<TextView>(R.id.textView)
+
+        fun setData(person: Person){
+            textView.text = person.name
+
+            Glide
+                .with(itemView.context)
+                .load(person.url)
+                .into(imageView)
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_person, parent, false)
+        return PersonViewHolder(itemView)
+    }
+
+    override fun getItemCount() = persons.size
+
+    override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
+        val person = persons[position]
+        holder.setData(persons[position])
+
+        holder.itemView.setOnClickListener {
+            onItemClick?.invoke(person)
+        }
+
+    }
+}
